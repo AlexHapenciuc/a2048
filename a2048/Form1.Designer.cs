a3884e3272e4493cb1960022665e32b9d9a277a6
AlexHapenciuc <80458940+AlexHapenciuc@users.noreply.github.com>
2021-03-11 19:54:21 +0200

    Add files via upload

---
+++ a2048/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace a2048
+{
+    partial class MyWForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tile1 = new System.Windows.Forms.Button();
+            this.tile2 = new System.Windows.Forms.Button();
+            this.tile3 = new System.Windows.Forms.Button();
+            this.tile4 = new System.Windows.Forms.Button();
+            this.tile5 = new System.Windows.Forms.Button();
+            this.tile7 = new System.Windows.Forms.Button();
+            this.tile6 = new System.Windows.Forms.Button();
+            this.tile8 = new System.Windows.Forms.Button();
+            this.tile9 = new System.Windows.Forms.Button();
+            this.tile10 = new System.Windows.Forms.Button();
+            this.tile12 = new System.Windows.Forms.Button();
+            this.tile13 = new System.Windows.Forms.Button();
+            this.tile15 = new System.Windows.Forms.Button();
+            this.tile14 = new System.Windows.Forms.Button();
+            this.tile11 = new System.Windows.Forms.Button();
+            this.tile16 = new System.Windows.Forms.Button();
+            this.Background = new System.Windows.Forms.PictureBox();
+            this.HighScores = new System.Windows.Forms.PictureBox();
+            this.NewGame = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Background)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HighScores)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NewGame)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tile1
+            // 
+            this.tile1.CausesValidation = false;
+            this.tile1.Enabled = false;
+            this.tile1.FlatAppearance.BorderSize = 0;
+            this.tile1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tile1.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tile1.Location = new System.Drawing.Point(15, 115);
+            this.tile1.Name = "tile1";
+            this.tile1.Size = new System.Drawing.Size(107, 107);
+            this.tile1.TabIndex = 3;
+            this.tile1.TabStop = false;
+            this.tile1.UseVisualStyleBackColor = true;
+            // 
+            // tile2
+            // 
+            this.tile2.CausesValidation = false;
+            this.tile2.Enabled = false;
+            this.tile2.FlatAppearance.BorderSize = 0;
+            this.tile2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tile2.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Bold);
+            this.tile2.Location = new System.Drawing.Point(136, 115);
+            this.tile2.Name = "tile2";
+            this.tile2.Size = new System.Drawing.Size(107, 107);
+            this.tile2.TabIndex = 4;
+            this.tile2.TabStop = false;
+            this.tile2.UseVisualStyleBackColor = true;
+            // 
+            // tile3
+            // 
+            this.tile3.CausesValidation = false;
+            this.tile3.Enabled = false;
+            this.tile3.FlatAppearance.BorderSize = 0;
+            this.tile3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tile3.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Bold);
+            this.tile3.Location = new System.Drawing.Point(257, 115);
+            this.tile3.Name = "tile3";
+            this.tile3.Size = new System.Drawing.Size(107, 107);
+            this.tile3.TabIndex = 5;
+            this.tile3.TabStop = false;
+            this.tile3.UseVisualStyleBackColor = true;
+            // 
+            // tile4
+            // 
+            this.tile4.CausesValidation = false;
+            this.tile4.Enabled = false;
+            this.tile4.FlatAppearance.BorderSize = 0;
+            this.tile4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tile4.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Bold);
+            this.tile4.Location = new System.Drawing.Point(378, 115);
+            this.tile4.Name = "tile4";
+            this.tile4.Size = new System.Drawing.Size(107, 107);
+            this.tile4.TabIndex = 6;
+            this.tile4.TabStop = false;
+            this.tile4.UseVisualStyleBackColor = true;
+            // 
+            // tile5
+            // 
+            this.tile5.CausesValidation = false;
+            this.tile5.Enabled = false;
+            this.tile5.FlatAppearance.BorderSize = 0;
+            this.tile5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tile5.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Bold);
+            this.tile5.Location = new System.Drawing.Point(15, 236);
+            this.tile5.Name = "tile5";
+            this.tile5.Size = new System.Drawing.Size(107, 107);
+            this.tile5.TabIndex = 7;
+            this.tile5.TabStop = false;
+            this.tile5.UseVisualStyleBackColor = true;
+            // 
+            // tile7
+            // 
+            this.tile7.CausesValidation = false;
+            this.tile7.Enabled = false;
+            this.tile7.FlatAppearance.BorderSize = 0;
+            this.tile7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tile7.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Bold);
+            this.tile7.Location = new System.Drawing.Point(257, 236);
+            this.tile7.Name = "tile7";
+            this.tile7.Size = new System.Drawing.Size(107, 107);
+            this.tile7.TabIndex = 8;
+            this.tile7.TabStop = false;
+            this.tile7.UseVisualStyleBackColor = true;
+            // 
+            // tile6
+            // 
+            this.tile6.CausesValidation = false;
+            this.tile6.Enabled = false;
+            this.tile6.FlatAppearance.BorderSize = 0;
+            this.tile6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tile6.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Bold);
+            this.tile6.Location = new System.Drawing.Point(136, 236);
+            this.tile6.Name = "tile6";
+            this.tile6.Size = new System.Drawing.Size(107, 107);
+            this.tile6.TabIndex = 9;
+            this.tile6.TabStop = false;
+            this.tile6.UseVisualStyleBackColor = true;
+            // 
+            // tile8
+            // 
+            this.tile8.CausesValidation = false;
+            this.tile8.Enabled = false;
+            this.tile8.FlatAppearance.BorderSize = 0;
+            this.tile8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tile8.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Bold);
+            this.tile8.Location = new System.Drawing.Point(378, 236);
+            this.tile8.Name = "tile8";
+            this.tile8.Size = new System.Drawing.Size(107, 107);
+            this.tile8.TabIndex = 10;
+            this.tile8.TabStop = false;
+            this.tile8.UseVisualStyleBackColor = true;
+            // 
+            // tile9
+            // 
+            this.tile9.CausesValidation = false;
+            this.tile9.Enabled = false;
+            this.tile9.FlatAppearance.BorderSize = 0;
+            this.tile9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tile9.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Bold);
+            this.tile9.Location = new System.Drawing.Point(15, 358);
+            this.tile9.Name = "tile9";
+            this.tile9.Size = new System.Drawing.Size(107, 107);
+            this.tile9.TabIndex = 11;
+            this.tile9.TabStop = false;
+            this.tile9.UseVisualStyleBackColor = true;
+            // 
+            // tile10
+            // 
+            this.tile10.CausesValidation = false;
+            this.tile10.Enabled = false;
+            this.tile10.FlatAppearance.BorderSize = 0;
+            this.tile10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tile10.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Bold);
+            this.tile10.Location = new System.Drawing.Point(136, 358);
+            this.tile10.Name = "tile10";
+            this.tile10.Size = new System.Drawing.Size(107, 107);
+            this.tile10.TabIndex = 12;
+            this.tile10.TabStop = false;
+            this.tile10.UseVisualStyleBackColor = true;
+            // 
+            // tile12
+            // 
+            this.tile12.CausesValidation = false;
+            this.tile12.Enabled = false;
+            this.tile12.FlatAppearance.BorderSize = 0;
+            this.tile12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tile12.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Bold);
+            this.tile12.Location = new System.Drawing.Point(378, 358);
+            this.tile12.Name = "tile12";
+            this.tile12.Size = new System.Drawing.Size(107, 107);
+            this.tile12.TabIndex = 14;
+            this.tile12.TabStop = false;
+            this.tile12.UseVisualStyleBackColor = true;
+            // 
+            // tile13
+            // 
+            this.tile13.CausesValidation = false;
+            this.tile13.Enabled = false;
+            this.tile13.FlatAppearance.BorderSize = 0;
+            this.tile13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tile13.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Bold);
+            this.tile13.Location = new System.Drawing.Point(15, 479);
+            this.tile13.Name = "tile13";
+            this.tile13.Size = new System.Drawing.Size(107, 107);
+            this.tile13.TabIndex = 15;
+            this.tile13.TabStop = false;
+            this.tile13.UseVisualStyleBackColor = true;
+            // 
+            // tile15
+            // 
+            this.tile15.CausesValidation = false;
+            this.tile15.Enabled = false;
+            this.tile15.FlatAppearance.BorderSize = 0;
+            this.tile15.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tile15.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Bold);
+            this.tile15.Location = new System.Drawing.Point(257, 479);
+            this.tile15.Name = "tile15";
+            this.tile15.Size = new System.Drawing.Size(107, 107);
+            this.tile15.TabIndex = 16;
+            this.tile15.TabStop = false;
+            this.tile15.UseVisualStyleBackColor = true;
+            // 
+            // tile14
+            // 
+            this.tile14.CausesValidation = false;
+            this.tile14.Enabled = false;
+            this.tile14.FlatAppearance.BorderSize = 0;
+            this.tile14.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tile14.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Bold);
+            this.tile14.Location = new System.Drawing.Point(136, 479);
+            this.tile14.Name = "tile14";
+            this.tile14.Size = new System.Drawing.Size(107, 107);
+            this.tile14.TabIndex = 17;
+            this.tile14.TabStop = false;
+            this.tile14.UseVisualStyleBackColor = true;
+            // 
+            // tile11
+            // 
+            this.tile11.CausesValidation = false;
+            this.tile11.Enabled = false;
+            this.tile11.FlatAppearance.BorderSize = 0;
+            this.tile11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tile11.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Bold);
+            this.tile11.Location = new System.Drawing.Point(257, 358);
+            this.tile11.Name = "tile11";
+            this.tile11.Size = new System.Drawing.Size(107, 107);
+            this.tile11.TabIndex = 18;
+            this.tile11.TabStop = false;
+            this.tile11.UseVisualStyleBackColor = true;
+            // 
+            // tile16
+            // 
+            this.tile16.CausesValidation = false;
+            this.tile16.Enabled = false;
+            this.tile16.FlatAppearance.BorderSize = 0;
+            this.tile16.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tile16.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Bold);
+            this.tile16.Location = new System.Drawing.Point(378, 479);
+            this.tile16.Name = "tile16";
+            this.tile16.Size = new System.Drawing.Size(107, 107);
+            this.tile16.TabIndex = 19;
+            this.tile16.TabStop = false;
+            this.tile16.UseVisualStyleBackColor = true;
+            // 
+            // Background
+            // 
+            this.Background.Image = global::a2048.Properties.Resources.Background;
+            this.Background.Location = new System.Drawing.Point(0, 100);
+            this.Background.Name = "Background";
+            this.Background.Size = new System.Drawing.Size(500, 500);
+            this.Background.TabIndex = 2;
+            this.Background.TabStop = false;
+            // 
+            // HighScores
+            // 
+            this.HighScores.BackgroundImage = global::a2048.Properties.Resources.High_Scores;
+            this.HighScores.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.HighScores.Location = new System.Drawing.Point(0, 0);
+            this.HighScores.Name = "HighScores";
+            this.HighScores.Size = new System.Drawing.Size(250, 100);
+            this.HighScores.TabIndex = 1;
+            this.HighScores.TabStop = false;
+            // 
+            // NewGame
+            // 
+            this.NewGame.BackgroundImage = global::a2048.Properties.Resources.New_Game;
+            this.NewGame.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.NewGame.Location = new System.Drawing.Point(250, 0);
+            this.NewGame.Name = "NewGame";
+            this.NewGame.Size = new System.Drawing.Size(250, 100);
+            this.NewGame.TabIndex = 0;
+            this.NewGame.TabStop = false;
+            this.NewGame.MouseClick += new System.Windows.Forms.MouseEventHandler(this.NewGame_MouseClick);
+            // 
+            // MyWForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 600);
+            this.Controls.Add(this.tile16);
+            this.Controls.Add(this.tile11);
+            this.Controls.Add(this.tile14);
+            this.Controls.Add(this.tile15);
+            this.Controls.Add(this.tile13);
+            this.Controls.Add(this.tile12);
+            this.Controls.Add(this.tile10);
+            this.Controls.Add(this.tile9);
+            this.Controls.Add(this.tile8);
+            this.Controls.Add(this.tile6);
+            this.Controls.Add(this.tile7);
+            this.Controls.Add(this.tile5);
+            this.Controls.Add(this.tile4);
+            this.Controls.Add(this.tile3);
+            this.Controls.Add(this.tile2);
+            this.Controls.Add(this.tile1);
+            this.Controls.Add(this.Background);
+            this.Controls.Add(this.HighScores);
+            this.Controls.Add(this.NewGame);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MyWForm";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MyWForm_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Background)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HighScores)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NewGame)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox HighScores;
+        private System.Windows.Forms.PictureBox NewGame;
+        private System.Windows.Forms.PictureBox Background;
+        private System.Windows.Forms.Button tile16;
+        private System.Windows.Forms.Button tile11;
+        private System.Windows.Forms.Button tile14;
+        private System.Windows.Forms.Button tile15;
+        private System.Windows.Forms.Button tile13;
+        private System.Windows.Forms.Button tile12;
+        private System.Windows.Forms.Button tile10;
+        private System.Windows.Forms.Button tile9;
+        private System.Windows.Forms.Button tile8;
+        private System.Windows.Forms.Button tile6;
+        private System.Windows.Forms.Button tile7;
+        private System.Windows.Forms.Button tile5;
+        private System.Windows.Forms.Button tile4;
+        private System.Windows.Forms.Button tile3;
+        private System.Windows.Forms.Button tile2;
+        private System.Windows.Forms.Button tile1;
+    }
+}
+
---
+++ a2048/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace a2048
+{
+    public partial class MyWForm : Form
+    {
+        int[,] m = new int[4, 4];
+        int col, lin;
+        
+
+        public MyWForm()
+        {
+            InitializeComponent();
+
+            NewBoard();
+            UpdateTilesValue();
+            UpdateTilesType();
+        }
+
+        public void NewBoard()
+        {
+            for (lin = 0; lin < 4; lin++)
+                for (col = 0; col < 4; col++)
+                    m[lin, col] = 0;
+            AddTile();
+            AddTile();
+        }
+        
+        public void UpdateTilesValue()
+        {
+            tile1.Text = "" + m[0,0];   tile2.Text = "" + m[0,1];   tile3.Text = "" + m[0,2];   tile4.Text = "" + m[0,3];
+            tile5.Text = "" + m[1,0];   tile6.Text = "" + m[1,1];   tile7.Text = "" + m[1,2];   tile8.Text = "" + m[1,3];
+            tile9.Text = "" + m[2,0];   tile10.Text = "" + m[2,1];  tile11.Text = "" + m[2,2];  tile12.Text = "" + m[2,3];
+            tile13.Text = "" + m[3,0];  tile14.Text = "" + m[3,1];  tile15.Text = "" + m[3,2];  tile16.Text = "" + m[3,3];
+        }
+
+        public void UpdateTilesType()
+        {
+            int temp, count;
+
+            foreach (Control x in this.Controls)
+                if (x is Button && x.Text != "High Scores" && x.Text != "New Game")
+                {
+                    count = 0;
+
+                    if (x.Text != "0")
+                        temp = Int32.Parse(x.Text);
+                    else temp = 1;
+
+                    while (temp > 1)
+                    {
+                        temp = temp / 2;
+                        count++;
+                    }
+                    switch (count)
+                    {
+                        case 0:
+                            x.BackgroundImage = Properties.Resources.tile0;
+                            break;
+                        case 1:
+                            x.BackgroundImage = Properties.Resources.tile1;
+                            break;
+                        case 2:
+                            x.BackgroundImage = Properties.Resources.tile2;
+                            break;
+                        case 3:
+                            x.BackgroundImage = Properties.Resources.tile3;
+                            break;
+                        case 4:
+                            x.BackgroundImage = Properties.Resources.tile4;
+                            break;
+                        case 5:
+                            x.BackgroundImage = Properties.Resources.tile5;
+                            break;
+                        case 6:
+                            x.BackgroundImage = Properties.Resources.tile6;
+                            break;
+                        case 7:
+                            x.BackgroundImage = Properties.Resources.tile7;
+                            break;
+                        case 8:
+                            x.BackgroundImage = Properties.Resources.tile8;
+                            break;
+                        case 9:
+                            x.BackgroundImage = Properties.Resources.tile9;
+                            break;
+                        case 10:
+                            x.BackgroundImage = Properties.Resources.tile10;
+                            break;
+                        case 11:
+                            x.BackgroundImage = Properties.Resources.tile11;
+                            break;
+                        case 12:
+                            x.BackgroundImage = Properties.Resources.tile12;
+                            break;
+                        default:
+                            x.BackgroundImage = Properties.Resources.tile1;//errortile
+                            break;
+                    }
+                } 
+        }
+
+        public void AddTile()
+        {
+            Random rnd = new Random();
+            int rndLin, rndCol, rndVal;
+
+            rndLin = rnd.Next(4);
+            rndCol = rnd.Next(4);
+            rndVal = rnd.Next(30);
+
+            while (m[rndLin, rndCol] != 0)
+            {
+                rndLin = rnd.Next(4);
+                rndCol = rnd.Next(4);
+            }
+
+            if (rndVal > 20)
+                m[rndLin, rndCol] = 4;
+            else
+                m[rndLin, rndCol] = 2;
+        }
+
+        public void MoveTilesUp()
+        {
+            int xlin;
+
+            for (col = 0; col < 4; col++)
+            {
+                xlin = 0;
+
+                for (lin = 1; lin < 4; lin++)
+                {
+                    if (m[lin, col] != 0)
+                    {
+                        if (m[lin, col] == m[xlin, col])
+                        {
+                            m[xlin, col] = m[xlin, col] * 2;
+                            m[lin, col] = 0;
+                        }
+                        else if (m[xlin, col] == 0)
+                        {
+                            m[xlin, col] = m[lin, col];
+                            m[lin, col] = 0;
+                            xlin--;
+                        }
+                        else if (lin - xlin > 1)
+                        {
+                            m[xlin + 1, col] = m[lin, col];
+                            m[lin, col] = 0;
+                        }
+                        xlin++;
+                    }
+                }
+            }
+        }
+
+        public void MoveTilesLeft()
+        {
+            int xcol;
+            for (lin = 0; lin < 4; lin++)
+            {
+                xcol = 0;
+                for (col = 1; col < 4; col++)
+                {
+                    if (m[lin, col] != 0)
+                    {
+                        if (m[lin, col] == m[lin, xcol])
+                        {
+                            m[lin, xcol] = m[lin, xcol] * 2;
+                            m[lin, col] = 0;
+                        }
+                        else if (m[lin, xcol] == 0)
+                        {
+                            m[lin, xcol] = m[lin, col];
+                            m[lin, col] = 0;
+                            xcol--;
+                        }
+                        else if (col - xcol > 1)
+                        {
+                            m[lin, xcol + 1] = m[lin, col];
+                            m[lin, col] = 0;
+                        }
+                        xcol++;
+                    }
+                }
+            }
+        }
+
+        public void MoveTilesRight()
+        {
+            int xcol;
+            for (int lin = 0; lin < 4; lin++)
+            {
+                xcol = 3;
+                for (int col = 2; col >= 0; col--)
+                {
+                    if (m[lin, col] != 0)
+                    {
+                        if (m[lin, col] == m[lin, xcol])
+                        {
+                            m[lin, xcol] = m[lin, xcol] * 2;
+                            m[lin, col] = 0;
+
+                        }
+                        else if (m[lin, xcol] == 0)
+                        {
+                            m[lin, xcol] = m[lin, col];
+                            m[lin, col] = 0;
+                            xcol++;
+                        }
+                        else if (xcol - col > 1)
+                        {
+                            m[lin, xcol - 1] = m[lin, col];
+                            m[lin, col] = 0;
+                        }
+                        xcol--;
+                    }
+                }
+            }
+        }
+
+        public void MoveTilesDown()
+        {
+            int xlin;
+
+            for (int col = 0; col < 4; col++)
+            {
+                xlin = 3;
+                for (int lin = 2; lin >= 0; lin--)
+                {
+                    if (m[lin, col] != 0)
+                    {
+                        if (m[lin, col] == m[xlin, col])
+                        {
+                            m[xlin, col] = m[xlin, col] * 2;
+                            m[lin, col] = 0;
+
+                        }
+                        else if (m[xlin, col] == 0)
+                        {
+                            m[xlin, col] = m[lin, col];
+                            m[lin, col] = 0;
+                            xlin++;
+                        }
+                        else if (xlin - lin > 1)
+                        {
+                            m[xlin - 1, col] = m[lin, col];
+                            m[lin, col] = 0;
+                        }
+                        xlin--;
+                    }
+                }
+            }
+        }
+
+        private void NewGame_MouseClick(object sender, MouseEventArgs e)
+        {
+            NewBoard();
+            UpdateTilesValue();
+            UpdateTilesType();
+        }
+
+        private void MyWForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) MoveTilesLeft();
+            if (e.KeyCode == Keys.Right) MoveTilesRight();
+            if (e.KeyCode == Keys.Down) MoveTilesDown();
+            if (e.KeyCode == Keys.Up) MoveTilesUp();
+
+            if (e.KeyCode == Keys.Escape) Application.Exit();
+
+            if (e.KeyCode == Keys.Down || e.KeyCode == Keys.Up || e.KeyCode == Keys.Left || e.KeyCode == Keys.Right)
+            {
+                AddTile();
+                UpdateTilesValue();
+                UpdateTilesType();
+            }
+        }
+    }
+}
